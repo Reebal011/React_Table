65abefcc3075defc9e4bd24590fca9825e2af8a2
Reebal Akram <reebalakram@gmail.com>
2022-11-03 12:42:18 +0500

    Goto Page in pagination

--- src/components/PaginationTable.js
+++ src/components/PaginationTable.js
@@ -19,11 +19,14 @@ const PaginationTable = () => {
     canNextPage,
     canPreviousPage,
     pageOptions,
+    gotoPage,
+    pageCount,
     state,
   } = useTable(
     {
       columns,
       data,
+      initialState: { pageIndex: 1 }, //to start page from 2
     },
     usePagination
   );
@@ -62,11 +65,31 @@ const PaginationTable = () => {
             {pageIndex + 1} of {pageOptions.length}
           </strong>
         </span>
+        <span>
+          | Go to page:
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageNumber = e.target.value
+                ? Number(e.target.value) - 1
+                : 0;
+              gotoPage(pageNumber);
+            }}
+            style={{ width: "50px" }}
+          />
+        </span>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>{" "}
         <button onClick={() => previousPage()} disabled={!canPreviousPage}>
           Back
         </button>
         <button onClick={() => nextPage()} disabled={!canNextPage}>
           Next
+        </button>{" "}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
         </button>
       </div>
     </>
